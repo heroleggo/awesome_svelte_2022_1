e042a7fe7db39f2d5fe138abf497af2f55d091db
heroleggo <h3r0.y0u@gmail.com>
2022-06-01 07:22:28 +0900

    :tada: init
    - added basic codes
    - changed font to SUIT

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,36 @@
+<script>
+</script>
+<style lang="scss">
+    .header {
+      padding: 10px 0;
+      display: flex;
+      justify-content: space-between;
+    }
+    .name {
+      padding-left: 10px;
+      font-size: 16px;
+      font-weight: 700;
+    }
+</style>
+
+<div class="header">
+    <div class="name">
+        Frontend, Backend Developer Youngwoong You
+    </div>
+    <nav>
+        <div class="">
+            <a href="/">home</a>
+            <a href="/about">about</a>
+            <a href="/projects">projects</a>
+        </div>
+    </nav>
+</div>
+
+<slot></slot>
+
+<footer>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/projects">projects</a>
+</footer>
+
