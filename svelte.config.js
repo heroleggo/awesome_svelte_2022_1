fe11d1e016fb615ad2701dd579568c133cb3e925
heroleggo <h3r0.y0u@gmail.com>
2022-06-01 06:39:36 +0900

    :wrench: updated configuration
    - scss 전역 variable 사용이 가능하도록 함
    - prettier 기본 설정 진행
    - 필요 패키지 설치
    - %svelte% 를 %sveltekit%으로 변경

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+  },
+  preprocess: preprocess({
+    scss: {
+      prependData: `@import 'src/style/_variables.scss';`,
+    },
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  }),
 };
 
 export default config;
