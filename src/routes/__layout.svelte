83ba3f33a419b60006b147c67b9c98b6abeb0a43
heroleggo <h3r0.y0u@gmail.com>
2022-06-06 01:58:37 +0900

    :art: applied prettier
    - 여태까지 Webstorm 세팅문제로 prettier 적용이 안되던 부분 수정

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,65 +1,19 @@
 <script>
     import { page } from '$app/stores';
 
-	let currentPath;
+    let currentPath;
 
     $: if ($page.url.pathname === '/') {
-		currentPath = 'home';
+        currentPath = 'home';
     } else if ($page.url.pathname === '/about') {
-		currentPath = 'about';
+        currentPath = 'about';
     } else if ($page.url.pathname === '/projects') {
-		currentPath = 'projects';
+        currentPath = 'projects';
     }
 </script>
-<style lang="scss">
-    .header {
-      width: 85%;
-      margin: auto;
-      padding: 12px 12px;
-      display: flex;
-      justify-content: space-between;
-      .name {
-        font-size: 20px;
-        font-weight: 700;
-      }
-      .menu {
-        width: 30%;
-        display: flex;
-        align-items: center;
-        justify-content: flex-end;
-        a {
-          margin: 0 12px;
-        }
-      }
-    }
-    .footer {
-      padding: 0 20px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      text-align: center;
-    }
-    .container {
-      padding: 10px 0;
-      background: linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%);
-      width: 100%;
-      height: 240px;
-      font-size: 22px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      & > div {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-      }
-    }
-</style>
 
 <div class="header">
-    <div class="name">
-        Junior Web Developer, Youngwoong You
-    </div>
+    <div class="name">Junior Web Developer, Youngwoong You</div>
     <div class="menu">
         <a href="/">home</a>
         <a href="/about">about</a>
@@ -73,17 +27,13 @@
             <p>주니어 웹 개발자 유영웅입니다.</p>
         </div>
     {:else if currentPath === 'about'}
-        <div>
-            About Me
-        </div>
+        <div>About Me</div>
     {:else if currentPath === 'projects'}
-        <div>
-            Projects
-        </div>
+        <div>Projects</div>
     {/if}
 </div>
 
-<slot></slot>
+<slot />
 
 <footer class="footer">
     <div>
@@ -92,3 +42,47 @@
     </div>
 </footer>
 
+<style lang="scss">
+    .header {
+        width: 85%;
+        margin: auto;
+        padding: 12px 12px;
+        display: flex;
+        justify-content: space-between;
+        .name {
+            font-size: 20px;
+            font-weight: 700;
+        }
+        .menu {
+            width: 30%;
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            a {
+                margin: 0 12px;
+            }
+        }
+    }
+    .footer {
+        padding: 0 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
+    .container {
+        padding: 10px 0;
+        background: linear-gradient(to top, #fbc2eb 0%, #a6c1ee 100%);
+        width: 100%;
+        height: 240px;
+        font-size: 22px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        & > div {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+    }
+</style>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,49 +1,10 @@
-<style lang="scss">
-  .about {
-    width: 80%;
-    margin: auto;
-  }
-    .icon-holder {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      h3 {
-        font-size: 24px;
-        font-weight: 600;
-      }
-      div {
-        border: 1px solid $gray-200;
-        border-radius: 5px;
-        padding: 20px;
-      }
-      div > a {
-        border: 0.5px solid $gray-100;
-        border-radius: 5px;
-        cursor: pointer;
-        width: 400px;
-        height: 80px;
-        display: flex;
-        align-items: center;
-        font-size: 20px;
-        font-weight: 700;
-
-        img {
-          margin-right: 10px;
-        }
-      }
-      div > a:not(:last-of-type) {
-        margin-bottom: 12px;
-    }
-      div > a:hover {
-        background: $gray-100;
-      }
-    }
-</style>
-
-
 <div class="about">
     <p>저는 대충 어쩌구저쩌구인 사람입니다.</p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, aliquid amet doloribus eligendi facilis soluta tenetur. Consectetur dignissimos dolores eaque earum eos excepturi hic iure magnam, nemo nihil porro, repellendus?</p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, aliquid amet doloribus eligendi facilis soluta
+        tenetur. Consectetur dignissimos dolores eaque earum eos excepturi hic iure magnam, nemo nihil porro,
+        repellendus?
+    </p>
     <div class="icon-holder">
         <h3>Links</h3>
         <div>
@@ -52,9 +13,49 @@
 
                 Go to Github
             </a>
-            <a href="https://heroleggo.world" target="_blank">
-                Go to Tistory Blog (almost empty :D)
-            </a>
+            <a href="https://heroleggo.world" target="_blank"> Go to Tistory Blog (almost empty :D) </a>
         </div>
     </div>
 </div>
+
+<style lang="scss">
+    .about {
+        width: 80%;
+        margin: auto;
+    }
+    .icon-holder {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        h3 {
+            font-size: 24px;
+            font-weight: 600;
+        }
+        div {
+            border: 1px solid $gray-200;
+            border-radius: 5px;
+            padding: 20px;
+        }
+        div > a {
+            border: 0.5px solid $gray-100;
+            border-radius: 5px;
+            cursor: pointer;
+            width: 400px;
+            height: 80px;
+            display: flex;
+            align-items: center;
+            font-size: 20px;
+            font-weight: 700;
+
+            img {
+                margin-right: 10px;
+            }
+        }
+        div > a:not(:last-of-type) {
+            margin-bottom: 12px;
+        }
+        div > a:hover {
+            background: $gray-100;
+        }
+    }
+</style>
